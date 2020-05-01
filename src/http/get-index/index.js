d1e3e8c90e928ed14cc1e6f5faf5f1eece47009c
Karsten Wolfert <karsten.wolfert@iw-experten.de>
2020-05-01 10:55:18 +0200

    Update web site title

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -8,7 +8,7 @@ exports.handler = async function Index () {
      * Basic bio
      */
     fullname: 'Karsten Wolfert', // ←  Start by adding your name!
-    title: 'My personal site!',
+    title: 'Karsten Wolfert - Systemarchitekt!',
     occupation: 'System Architect',
     location: 'West Glacier, MT',
     bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Quis ipsum suspendisse ultrices gravida. Risus commodo viverra maecenas accumsan lacus vel facilisis.',
